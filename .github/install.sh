c57aa3ac808a67c17d4d9af22e3696efbf63c13d
Gerard Braad <me@gbraad.nl>
2025-02-22 13:43:00 +0800

    Initial version

---
+++ .github/install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# clean workspace folder
+rm -rf /workspaces/debian-golang
+mkdir /workspaces/debian-golang
+ln -s /workspaces/debian-golang ~/Projects
+git init /workspaces/debian-golang
+
+cd ~
